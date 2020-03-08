22265bf31a70c26a989e05dc941371fedd8aa09b
mango <690852195@qq.com>
2020-03-08 16:22:27 +0800

    update

--- 申请后台时间/AppDelegate.m
+++ 申请后台时间/AppDelegate.m
@@ -11,77 +11,66 @@
 @interface AppDelegate ()
 
 @end
-
+/**
+申请的后台任务标识
+app进入后台时, 申请后台任务, 返回后台任务标识
+app从后台返回前台时, 如果申请的后台任务还没结束, 需要结束后台任务
+*/
+static UIBackgroundTaskIdentifier _bgTask;
 @implementation AppDelegate
 
+ 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+  
     return YES;
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
 
 -(void)testBackGroundTimeLen{
     
 //    每隔1 秒写一个信息 测试到本地文件
+    NSLog(@"=========");
     
 }
 
-// 了解一下 http://www.mamicode.com/info-detail-2249729.html
+ // https://www.jianshu.com/p/f1b3cda7a61f
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-   [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(testBackGroundTimeLen) userInfo:nil repeats:YES];
+    [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(testBackGroundTimeLen) userInfo:nil repeats:YES];
     
     
-    
-    // 开始申请额外时间, 拿借据
-    __block UIBackgroundTaskIdentifier bgTask;
-    bgTask = [[UIApplication sharedApplication]  beginBackgroundTaskWithExpirationHandler:^{
+    // 申请后台任务(一般为3分钟)
+    _bgTask= [[UIApplication sharedApplication]  beginBackgroundTaskWithExpirationHandler:^{
         
+        /**
+         当你想app申请的后台任务时间快结束了, 就会回调这个block块, 你需要在这个blcok内做一些
+         清理收尾方面的工作, 否则可能会导致程序出错.
+         这个block 会在主线程调用, 因此你可以在这个block内放心大胆的处理你的收尾工作
+         */
         
-        // 时间到了,还没完, 强行还借据
-        ////在ios7以前，后台可以用下面的的方式，去在后台存活5-10分钟，在ios8中，只能存活3分钟。
-        // 如果在系统规定时间内任务还没有完成，在时间到之前会调用到这个方法，一般是10分钟
-        dispatch_async(dispatch_get_main_queue(), ^{
-            [[UIApplication sharedApplication] endBackgroundTask:bgTask];
-            if (bgTask != UIBackgroundTaskInvalid) {
-                bgTask = UIBackgroundTaskInvalid;
-            }
-        });
+        NSLog(@"申请的后台任务时间 快结束了, 赶紧在这个block内处理你的收尾工作, %@",[NSThread currentThread]);
+        
+        [[UIApplication sharedApplication] endBackgroundTask:_bgTask];
+        if (_bgTask != UIBackgroundTaskInvalid) {
+            _bgTask = UIBackgroundTaskInvalid;
+        }
     }];
     
-    
-    // 开始做自己的后台完善工作
-    // ....
-    
-    // 后台工作完善后 还借据
-    [[UIApplication sharedApplication] endBackgroundTask:bgTask];
-    bgTask = UIBackgroundTaskInvalid;
-    
-    
-
 }
 
 
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
 
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+-(void)applicationWillEnterForeground:(UIApplication *)application{
+    
+    //app从后台返回前台时, 如果申请的后台任务还没结束, 需要结束后台任务
+    if (_bgTask != UIBackgroundTaskInvalid) {
+        [[UIApplication sharedApplication] endBackgroundTask:_bgTask];
+        _bgTask  = UIBackgroundTaskInvalid;
+    }
+    
+    
 }
 
 
